16309b0397dfa267233380af1a732f040511d955
Maite Perez Vento <maitepv87@gmail.com>
2025-09-29 15:15:43 -0400

    Add zustand

--- src/app/shop/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="">
-      <h1>HOLA MUNDO</h1>
-    </main>
-  );
-}
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { titleFont } from "@/config/fonts";
 import Link from "next/link";
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
+import { useUIStore } from "@/store";
 
 export const TopMenu = () => {
-  // const openSideMenu = useUIStore( state => state.openSideMenu );
+  const openSideMenu = useUIStore((state) => state.openSideMenu);
 
   return (
     <nav className="flex px-5 justify-between items-center w-full">
@@ -55,7 +58,7 @@ export const TopMenu = () => {
         </Link>
 
         <button
-          // onClick={ openSideMenu }
+          onClick={openSideMenu}
           className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
         >
           Menu
